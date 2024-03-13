c0e4dab88037b3f6f3f9bfe5d4cc765c9f869a19
Renato Balbinotti dos Santos <renatobalbinottisantos@gmail.com>
2024-03-13 01:55:32 -0300

    Aula 11
    Aula utilizando Alert, Prompt e Confirm

--- aula11/Js/script.js
+++ aula11/Js/script.js
@@ -1,1 +1,7 @@
-alert('Alert com a nossa mensagem');
+let num1 = prompt('Digite um número:');
+let num2 = prompt('Digite outro número');
+
+num1 = Number(num1);
+num2 = Number(num2);
+
+alert(`O resultado da sua conta foi: ${num1 + num2}`);
