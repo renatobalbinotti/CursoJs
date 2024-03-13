00a1ff1d9629b099a6e8eac947046e46d868d36b
Renato Balbinotti dos Santos <renatobalbinottisantos@gmail.com>
2024-03-13 02:29:32 -0300

    Aula 15 - Biblioteca Math
    Utilizando a biblioteca Math para manipulação de valores Numbers.

---
+++ aula15/script.js
@@ -0,0 +1,12 @@
+console.log(!!(100 / 0));
+
+// let num2 = Math.floor(8.547); //Arredonda para baixo
+// let num2 = Math.ceil(8.547); // Arredonda para cima
+// let num2 = Math.round(8.547); // Arredonda pra o número mais perto (Ex: 7,8 -> 8// 7.2 - > 7)
+
+
+// console.log(Math.max(1,2,3,4,5,-10,-50,1500,9,8,7,6)); // Busca o maior número dentre os selecionados
+// console.log(Math.min(1,2,3,4,5,-10,-50,1500,9,8,7,6)); // Busca o menor número dentre os selecionados.
+
+// const aleatorio = Math.round(Math.random() * (10 - 5) + 5);
+
