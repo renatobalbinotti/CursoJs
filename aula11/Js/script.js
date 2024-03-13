2f6c16de29e679ceada705f555929525d6920c38
Renato Balbinotti dos Santos <renatobalbinottisantos@gmail.com>
2024-03-13 02:03:27 -0300

    Aula 11
    Aula utilizando alert, confirm e prompt

--- aula11/Js/script.js
+++ aula11/Js/script.js
@@ -1,7 +1,12 @@
-let num1 = prompt('Digite um número:');
-let num2 = prompt('Digite outro número');
+let confirmacao = confirm('Você deseja fazer a soma de dois números?')
 
-num1 = Number(num1);
-num2 = Number(num2);
+if (confirmacao) {
 
-alert(`O resultado da sua conta foi: ${num1 + num2}`);
+    let num1 = prompt('Digite um número:');
+    let num2 = prompt('Digite outro número');
+
+    num1 = Number(num1);
+    num2 = Number(num2);
+
+    alert(`O resultado da sua conta foi: ${num1 + num2}`);
+};
