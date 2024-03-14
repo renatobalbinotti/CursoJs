6963d2da9f81b0dcd09db691f1259c8e68fad3cf
Renato Balbinotti dos Santos <renatobalbinottisantos@gmail.com>
2024-03-13 23:21:59 -0300

    Aula 21 - Operadores de Comparação

---
+++ aula 21 - Operadores de comparação/index.js
@@ -0,0 +1,43 @@
+/**
+ * Operadores de comparação
+ * > maior que
+ * >= maior ou igual a 
+ * < menor que
+ * <= menor ou igual a 
+ * == igualdade (valor)
+ * === igualdade estrita (valor e tipo)
+ * != diferente (valor)
+ * !== diferente estrito (valor e tipo)
+ */
+
+//Usando > maior que
+let comp = 10 > 5;
+console.log(comp);
+
+//Usando >= maior ou igual a 
+comp = 10 >= 11;
+console.log(comp);
+
+//Usando < menor que
+comp = 10 < 11;
+console.log(comp);
+
+//Usando <= menor ou igual a 
+comp = 12 <= 11;
+console.log(comp);
+
+//Usando == igualdade (valor)
+comp = 12 == '12'
+console.log(comp)
+
+//Usando === igualdade estrita (valor e tipo)
+comp = 12 === '12'
+console.log(comp)
+
+//Usando != diferente (valor)
+comp = 12 != 12
+console.log(comp)
+
+//Usando !== diferente estrito (valor e tipo)
+comp = 12 !== '12'
+console.log(comp)
