3bcc633501652db98da5b7f5df6192de9c9983dd
Renato Balbinotti dos Santos <Renatobalbinottisantos@gmail.com>
2024-04-12 03:02:29 -0300

    Aula 79 - Factory Functions + prototype

---
+++ Aula 79 - Factory Functions + prototype/index.js
@@ -0,0 +1,29 @@
+const falar = {
+  falar() {
+    console.log(`${this.nome} está falando`);
+  },
+};
+const comer = {
+  comer() {
+    console.log(`${this.nome} está comendo`);
+  },
+};
+const beber = {
+  beber() {
+    console.log(`${this.nome} está bebendo`);
+  },
+};
+
+// const pessoaPrototype = { ...falar, ...beber, ...comer };
+Object.assign(pessoaPrototype, falar, beber, comer);
+
+function criaPessoa(nome, sobrenome) {
+  return Object.create(pessoaPrototype, {
+    nome: { value: nome },
+    sobrenome: { value: sobrenome },
+  });
+}
+
+const p1 = new criaPessoa("Renato", "Balbinotti");
+console.log(p1);
+p1.falar();
